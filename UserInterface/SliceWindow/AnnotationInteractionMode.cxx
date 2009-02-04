0c9f7ecca4bf9768f72ff7277f2b7eeb3ead1c3b
garyhuizhang <garyhuizhang>
2009-02-04 00:25:28 +0000

    ENH: improve the robustness of the annotation mode

--- UserInterface/SliceWindow/AnnotationInteractionMode.cxx
+++ UserInterface/SliceWindow/AnnotationInteractionMode.cxx
@@ -3,8 +3,8 @@
   Program:   ITK-SNAP
   Module:    $RCSfile: AnnotationInteractionMode.cxx,v $
   Language:  C++
-  Date:      $Date: 2009/02/03 22:30:26 $
-  Version:   $Revision: 1.3 $
+  Date:      $Date: 2009/02/04 00:25:28 $
+  Version:   $Revision: 1.4 $
   Copyright (c) 2007 Paul A. Yushkevich
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
@@ -49,9 +49,16 @@ AnnotationInteractionMode
         {
         m_Lines.pop_back();
         }
-    m_Parent->GetCanvas()->redraw();
-    return 1;
     }
+  else if(Fl::event_button() == FL_LEFT_MOUSE)
+    {
+    // Record the location
+    Vector3d xEvent = to_double(m_Parent->MapWindowToSlice(event.XSpace.extract(2)));
+    m_CurrentLine.second = xEvent;
+    }
+
+  // Redraw
+  m_Parent->GetCanvas()->redraw();
 
   // Even though no action may have been performed, we don't want other handlers
   // to get the left and right mouse button events
@@ -63,6 +70,10 @@ AnnotationInteractionMode
 ::OnMouseDrag(const FLTKEvent &event,
               const FLTKEvent &pressEvent)
 {
+  // Make sure it's left mouse button being pressed
+  if(Fl::event_button() != FL_LEFT_MOUSE)
+    return 1;
+
   // Record the location
   Vector3d xEvent = to_double(m_Parent->MapWindowToSlice(event.XSpace.extract(2)));
 
