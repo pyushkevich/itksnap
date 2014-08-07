cf04f64ecbeca4e840212929bce6cf03fd07aa8a
Paul Yushkevich <pyushkevich@gmail.com>
2014-08-07 10:57:01 -0400

    added 3.2 dashboard

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,10 +4,10 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(Dart)
-set(CTEST_PROJECT_NAME "ITK-SNAP 3.0")
-set(CTEST_NIGHTLY_START_TIME "01:00:00 EST")
+set(CTEST_PROJECT_NAME "ITK-SNAP 3.2")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "itksnap.org")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=ITK-SNAP+3.0")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=ITK-SNAP+3.2")
 set(CTEST_DROP_SITE_CDASH TRUE)
