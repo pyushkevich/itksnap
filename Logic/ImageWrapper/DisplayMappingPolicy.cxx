32fa3652ab439a514c36b4021e563b38f97ba064
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2015-03-12 14:08:06 +0100

    using LinearColorMapDisplayMappingPolicy, basically works but bound to fixed intensity range

--- Logic/ImageWrapper/DisplayMappingPolicy.cxx
+++ Logic/ImageWrapper/DisplayMappingPolicy.cxx
@@ -948,7 +948,7 @@ MultiChannelDisplayMappingPolicy<TWrapperTraits>
 
 
 template class ColorLabelTableDisplayMappingPolicy<LabelImageWrapperTraits>;
-template class ColorLabelTableDisplayMappingPolicy<JsrcImageWrapperTraits>;
+template class LinearColorMapDisplayMappingPolicy<JsrcImageWrapperTraits>;
 
 template class LinearColorMapDisplayMappingPolicy<LevelSetImageWrapperTraits>;
 template class LinearColorMapDisplayMappingPolicy<SpeedImageWrapperTraits>;
