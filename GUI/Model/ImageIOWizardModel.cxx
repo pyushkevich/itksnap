4ad09a094dff8193bbe08351dd6962cf4d6a1145
JLasserv <56830768+JLasserv@users.noreply.github.com>
2020-04-30 11:46:40 -0400

    Add files via upload

--- GUI/Model/ImageIOWizardModel.cxx
+++ GUI/Model/ImageIOWizardModel.cxx
@@ -384,35 +384,6 @@ void ImageIOWizardModel::Reset()
 
 
 #include "miniunz.h"
-string ImageIOWizardModel::GetTempDirName()
-{
-#ifdef WIN32
-  char tempDir[_MAX_PATH + 1] = "";
-  char tempFile[_MAX_PATH + 1] = "";
-
-  // First call return a directory only
-  DWORD length = GetTempPath(_MAX_PATH+1, tempDir);
-  if(length <= 0 || length > _MAX_PATH)
-    throw IRISException("Unable to create temporary directory");
-
-  // This will create a unique file in the temp directory
-  if (0 == GetTempFileName(tempDir, TEXT("ZIPDIR"), 0, tempFile))
-    throw IRISException("Unable to create temporary directory");
-
-  // We use the filename to create a directory
-  string dir = tempFile;
-  dir += "_d";
-  _mkdir(dir.c_str());
-  remove(tempFile);
-  return dir;
-
-#else
-  char tmp_template[4096];
-  strcpy(tmp_template, "/tmp/ZIPDIR_XXXXXX");
-  string tmpdir = mkdtemp(tmp_template);
-  return tmpdir;
-#endif
-}
 
 
 void ImageIOWizardModel::ProcessDicomDirectory(const std::string &filename,
@@ -423,15 +394,23 @@ void ImageIOWizardModel::ProcessDicomDirectory(const std::string &filename,
 
   // Unzip and update the directory
   if(filename.find(".zip") != std::string::npos){
-      string temp = GetTempDirName();
+      IRISApplication *app;
+      string temp = app->GetTempDirName();
       chdir(temp.c_str());
 
       // Extract in a temporary directory
       const string file_extracted = extract_zip(filename.c_str());
       const string folder_extracted = file_extracted.substr(0, file_extracted.find_last_of("/\\"));
 
-      // Get the absolude path of the last extracted file
-      dir = temp + "/" + folder_extracted;
+      // Get the absolude path of the folder containing dicom images
+      if(folder_extracted.find(".dcm") != string::npos)
+        {
+        dir = temp;
+        }
+      else
+        {
+        dir = temp + "/" + folder_extracted;
+        }
 
       // Store both file paths
       m_map_zip["DICOMdir"] = dir;
