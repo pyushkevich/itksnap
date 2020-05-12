48ce2e82f24b5a9a41363165d1f4bad07994f11b
JLasserv <56830768+JLasserv@users.noreply.github.com>
2020-05-12 09:39:53 -0400

    Zipping methods added

---
+++ Common/Minizip/minizip.h
@@ -0,0 +1,40 @@
+#ifndef _minizip_H
+#define _minizip_H
+
+#include "zip.h"
+
+#define WRITEBUFFERSIZE (16384)
+#define MAXFILENAME (256)
+
+
+#ifdef __cplusplus
+#include <string>
+#include <vector>
+
+int CreateZIPFile(std::string ZIPfilename, std::vector<char*> files_to_zip);
+int ZipAFolder(const char* zipname, const char* folder);
+
+extern "C" {
+#endif
+
+#ifdef __APPLE__
+#define unix
+#endif
+
+#ifdef WIN32
+uLong filetime(char *f, tm_zip *tmzip, uLong *dt);
+#else
+#ifdef unix
+uLong filetime(char *f, tm_zip *tmzip, uLong *dt);
+#endif
+
+int check_exist_file(const char* filename);
+//void do_banner();
+//void do_help();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+#endif
