ad160ee4b9dc7343f1055cb6af9fc62ed01609d9
Jilei Hao <jilei.cs@gmail.com>
2021-07-13 22:01:18 -0400

    Fixed the 4D image DSS uploading issue

--- Logic/ImageWrapper/GuidedNativeImageIO.cxx
+++ Logic/ImageWrapper/GuidedNativeImageIO.cxx
@@ -859,7 +859,7 @@ GuidedNativeImageIO
   ImageBase *native = this->GetNativeImage();
 
   // Get the native image
-  typedef itk::VectorImage<TNative, 3> InputImageType;
+  typedef itk::VectorImage<TNative, 4> InputImageType;
   typename InputImageType::Pointer input = 
     reinterpret_cast<InputImageType *>(native);
   assert(input);
@@ -911,7 +911,7 @@ GuidedNativeImageIO
   ImageBase *native = this->GetNativeImage();
 
   // Get the native image
-  typedef itk::VectorImage<TNative, 3> InputImageType;
+  typedef itk::VectorImage<TNative, 4> InputImageType;
   typename InputImageType::Pointer input = 
     reinterpret_cast<InputImageType *>(native);
   assert(input);
