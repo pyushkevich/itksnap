afe7e7c5f26b8deef48132f35a936ab18ec2c62a
jilei-hao <jilei.cs@gmail.com>
2021-09-02 23:22:46 -0400

    czi-102: testing scripts

--- Testing/GUI/Qt/Scripts/test_4DContinuousRendering.js
+++ Testing/GUI/Qt/Scripts/test_4DContinuousRendering.js
@@ -5,4 +5,10 @@ include("Library");
 // Open the test workspace
 openWorkspace("img4d_11f.itksnap");
 
+// Find continuous update and triggers
+var actionContUpdate = engine.findChild(mainwin,"actionContinuous_Update");
+actionContUpdate.trigger();
+
+// scroll through frames
+
 engine.sleep(3000);
